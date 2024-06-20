9e372f452a94c71449dc2c099abbcdfe4bbcda12
ameliabaier17 <112589589+ameliabaier17@users.noreply.github.com>
2024-06-20 15:28:30 -0400

    index=False

--- website/dashboard/targeted_city_analysis.py
+++ website/dashboard/targeted_city_analysis.py
@@ -365,4 +365,4 @@ if view_option == "Among environmental and civil rights nonprofits":
 st.subheader("What nonprofits fall into clusters identified as having highest potential financial impact?")
 df_names_highfinance = df_env_city_cluster[(df_env_city_cluster['CLUSTER_KMEANS']=='1') | (df_env_city_cluster['CLUSTER_KMEANS']=='2')]
 df_names_highfinance = df_names_highfinance[['NAME', 'CITY', 'NTEE_NAME','CLUSTER_KMEANS']]
-st.dataframe(df_names_highfinance)
+st.dataframe(df_names_highfinance, index=False)
